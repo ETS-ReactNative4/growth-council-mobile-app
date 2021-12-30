de59c6e01ef97106212e12c110c60c28457ec6ff
Krishna Timilsina <krishna.timilsina@citytech.global>
2021-12-30 15:07:02 +0545

    improvements

--- src/screens/auth/SignUp.js
+++ src/screens/auth/SignUp.js
@@ -1,39 +1,42 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import SignUpForm from './components/SignUp';
 import {signUpCustomer, resetCustomer} from './authSlice';
 
 const SignUpScreen = props => {
-  const {entities, loading, error} = useSelector(state => state.auth);
+  
+    const dispatch = useDispatch();
 
-  /**
-   * Sign up customer data.
-   * @param {object} formData
-   *
-   */
-  const registerCustomer = formData => {
-    return dispatch(signUpCustomer(formData));
-  };
+    const {entities, loading, error} = useSelector(state => state.auth);
 
-  /**
-   * Clear customer data.
-   *
-   */
-  const cleanCustomer = () => {
-    dispatch(resetCustomer());
-  };
+    /**
+     * Sign up customer data.
+     * @param {object} formData
+     *
+     */
+    const registerCustomer = formData => {
+        return dispatch(signUpCustomer(formData));
+    };
 
-  return (
-    <SignUpForm
-      {...props}
-      customers={entities}
-      loading={loading}
-      error={error}
-      registerCustomer={registerCustomer}
-      cleanCustomer={cleanCustomer}
-    />
-  );
+    /**
+     * Clear customer data.
+     *
+     */
+    const cleanCustomer = () => {
+        dispatch(resetCustomer());
+    };
+
+    return (
+        <SignUpForm
+            {...props}
+            customers={entities}
+            loading={loading}
+            error={error}
+            registerCustomer={registerCustomer}
+            cleanCustomer={cleanCustomer}
+        />
+    );
 };
 
 export default SignUpScreen;
