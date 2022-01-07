f1de0561db2bb5c6493705609942351749241fe1
Krishna Timilsina <krishna.timilsina@citytech.global>
2022-01-07 16:36:10 +0545

    improvements

--- src/screens/setting/index.js
+++ src/screens/setting/index.js
@@ -1,36 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import Setting from './components/index';
+
+import Setting from './components';
 import {fetchProfileByID, resetProfile} from '../account/slice/profileSlice';
 
-
 const SettingScreen = (props) => {
-	const dispatch = useDispatch();
-	const {profile, profileLoading, profileError} = useSelector((state) => state.profile);
+    const dispatch = useDispatch();
 
-	const fetchProfile = () => {
+    const {profile, profileLoading, profileError} = useSelector((state) => state.profile);
+
+    const fetchProfile = () => {
         dispatch(fetchProfileByID());
     };
 
     const cleanProfile = () => {
         dispatch(resetProfile());
     };
-	return (
-		<Setting
-		{...props}
 
-		profile={profile}
-		profileLoading={profileLoading}
-		profileError={profileError}
-		fetchProfile={fetchProfile}
-		cleanProfile={cleanProfile}
-		/>
-	)
-}
+    return (
+        <Setting
+            {...props}
+
+            profile={profile}
+            profileLoading={profileLoading}
+            profileError={profileError}
+            fetchProfile={fetchProfile}
+            cleanProfile={cleanProfile}
+        />
+    )
+};
 
 export default SettingScreen;
-
-const styles = StyleSheet.create({
-
-})
