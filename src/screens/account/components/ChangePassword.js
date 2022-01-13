042c2b782cbde268fe6a34eeec71b446c5aeffd4
Reshika Shrestha <shresthareshika@gmail.com>
2022-01-13 21:12:01 +0545

    change password design

--- src/screens/account/components/ChangePassword.js
+++ src/screens/account/components/ChangePassword.js
@@ -5,6 +5,8 @@ import {
     View,
     StatusBar,
     TouchableOpacity,
+	ScrollView,
+	Image
 } from 'react-native';
 import {Button} from 'native-base';
 import {useFormik} from 'formik';
@@ -35,7 +37,12 @@ const passwordSchema = Yup.object().shape({
 
 const ChangePasswordForm = (props) => {
 
-    const {navigation, loading, error, updateCustomerPassword, cleanCustomerPassword} = props;
+    const {navigation, loading, error, updateCustomerPassword, cleanCustomerPassword,
+		profile,
+        profileLoading,
+        profileError,
+        fetchProfileByIdentifier,
+        cleanProfile,} = props;
 
     const [hidePass, setHidePass] = useState(true);
     const [hidePass1, setHidePass1] = useState(true);
@@ -73,10 +80,39 @@ const ChangePasswordForm = (props) => {
         },
         [],
     );
+	useEffect(() => {
+        const fetchProfileAsync = async () => {
+            await fetchProfileByIdentifier();
+        };
+        fetchProfileAsync();
+    }, []);
+
 
     return (
 
-        <View style={styles.container}>
+		<ScrollView contentContainerStyle={{flexGrow: 1,backgroundColor:Colors.PRIMARY_BACKGROUND_COLOR}}>
+		<View style={{backgroundColor:Colors.PRIMARY_BACKGROUND_COLOR}}>
+	
+			<Image source={require("../../../assets/img/appBG.png")} style={{height:160}}/>
+			
+					<View style={{display:'flex', marginTop:-90,alignContent:'center', marginLeft:'auto', marginRight:'auto'}}>
+							<View style={styles.profileWrapper}>
+									<View style={styles.icon}>
+										<Image source={{uri: profile.avatar}} style={{width:"100%", height:"100%"}} resizeMode='cover'
+										/>
+									</View>
+									<View style={styles.header}>
+										<Text style={styles.headingText1}>{profile.display_name}</Text>
+										<Text>{profile.user_email}</Text>
+									</View>
+							</View>
+					</View>
+					
+			
+			
+		</View>
+
+		<View style={styles.container}>
 
             <StatusBar barStyle="light-content" backgroundColor={Colors.PRIMARY_BACKGROUND_COLOR}/>
 
@@ -176,7 +212,7 @@ const ChangePasswordForm = (props) => {
 
                 <View style={styles.buttonWrapper}>
                     <Button style={styles.button} onPress={handleSubmit} disabled={!isValid}>
-                        <Text style={styles.buttonText}>Change | Update</Text>
+                        <Text style={styles.buttonText}>Update | Change</Text>
                     </Button>
                 </View>
 
@@ -187,6 +223,18 @@ const ChangePasswordForm = (props) => {
                 </View>
             </View>
         </View>
+		<View style={{ alignItems:'center', width:'35%',marginLeft:140, marginBottom:10}}>
+			<Text style={{fontSize: 8, marginTop: 10}}>Powered By</Text>
+			<Image 
+				source={require('../../../assets/img/fristDigi.png')}
+				style={{width:"100%", height:20}}
+			/>
+		</View>
+	
+		
+	</ScrollView>
+
+        
     );
 };
 
@@ -198,7 +246,10 @@ const styles = StyleSheet.create({
         ...CommonStyles.content,
     },
     body: {
-        ...CommonStyles.body,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+		borderRadius:20,
     },
     message: {
         ...CommonStyles.message,
@@ -208,7 +259,11 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
     button: {
-        ...CommonStyles.button,
+        width: '80%',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.PRIMARY_BUTTON_COLOR,
         height: 56,
     },
     buttonText: {
@@ -246,6 +301,150 @@ const styles = StyleSheet.create({
         fontFamily: Typography.FONT_MEDIUM,
         paddingLeft: 8,
     },
+	container: {
+        ...CommonStyles.container,
+        backgroundColor: Colors.SECONDARY_BACKGROUND_COLOR,
+        width: "100%",
+        height: '100%',
+    },
+ 
+ 
+    middleWrapper: {
+        height: 60,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+	TextWrapper: {
+        height: 380,
+        // backgroundColor:'green'
+        marginTop: 10,
+		
+    },
+
+   
+   
+    input: {
+        height: 40,
+        margin: 10,
+        borderWidth: 0.5,
+        padding: 10,
+        borderRadius: 10,
+    },
+
+   loginButtonWrapper: {
+        marginLeft: 10,
+		marginTop:18,
+      
+    }, 
+    loginButton: {
+        width: '50%',
+        borderRadius: 10,
+        height: 50,
+        backgroundColor: '#3A9BDC',
+    },
+    loginButtonText: {
+        color: Colors.PRIMARY_BUTTON_TEXT_COLOR,
+        fontFamily: Typography.FONT_BOLD,
+
+    },
+    message: {
+        ...CommonStyles.message,
+        width: '86%',
+    },
+    errorWrapper: {
+        width: '70%',
+    },
+    errorText: {
+        ...CommonStyles.errorText,
+    },
+
+	container: {
+        ...CommonStyles.container,
+        backgroundColor: Colors.PRIMARY_BACKGROUND_COLOR,
+		paddingLeft:50,
+		paddingRight:50,
+		
+    },
+	profileWrapper:{
+		padding:20,
+		alignItems:"center", 
+		width:328,
+		backgroundColor:Colors.PRIMARY_BACKGROUND_COLOR,
+		borderRadius:12, 
+		position:"relative",
+		paddingTop:100, 
+		borderWidth: 1 , 
+		borderColor:'#707070'
+	},
+    header: {    
+     alignItems:'center',
+    },
+    icon: {
+        width: 110,
+        height: 110,
+        borderColor:Colors.PRIMARY_BACKGROUND_COLOR,
+        borderRadius: 16,
+        borderWidth: 3,
+		overflow:"hidden",
+		position:"absolute",
+		top:-35,
+    },
+    headingText1: {
+        ...CommonStyles.headingText1,
+        fontFamily: Typography.FONT_NORMAL,
+        fontSize: 22,
+        fontWeight: '600',
+    },
+    middle: {
+     
+    },
+    wrapper: {
+        marginTop: 35,
+		borderBottomWidth:1 ,
+		
+		borderBottomColor:'#EDF1F7',
+    },
+    middleWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+		paddingTop:15,
+		paddingBottom:15,
+		borderBottomWidth:1 ,
+		alignItems:'center',
+		borderBottomColor:'#EDF1F7',
+		position:'relative',
+    },
+	TextWrapper: {
+        height: 380,
+        // backgroundColor:'green'
+        marginTop: 10,
+    },
+
+    middleImage: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#3A9BDC',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginLeft: 10,
+       
+    },
+    middleImage1: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#d7d7d7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginLeft: 10,
+    },
+	menuText:{
+		fontSize: 14, 
+		fontWeight: '500',
+		margin: 15
+	}
+
 });
 
 
--- src/screens/people/components/index.js
+++ src/screens/people/components/index.js
@@ -75,13 +75,13 @@ const People = (props) => {
                         keyboardType="text"	
                     />
                     <Ionicons name='list-outline' color="#14A2E2" size={30} style={{marginTop: 10}}/>
-                    <Ionicons name='apps' color={'#000'} size={30} style={{marginLeft: 10, marginTop: 15}}/>
+                    <Ionicons name='apps' color={'#000'} size={30} style={{marginLeft: 10, marginTop: 10}}/>
 
                 </View>
                 <View style={{display: 'flex', flexDirection: 'row', height:48, marginTop:16}}>
 
 				
-					<View style={{borderRightWidth:1,borderColor:'#707070'}}>
+					<View style={{borderRightWidth:0.2,borderColor:'#707070'}}>
 						<Picker
 							selectedValue={category}
 							mode={'dropdown'}
@@ -94,7 +94,7 @@ const People = (props) => {
 						</Picker>
 					</View>
                   
-					<View style={{borderRightWidth:1,borderColor:'#707070', display:'flex', flexDirection:'row'}}>
+					<View style={{borderRightWidth:0.2,borderColor:'#707070', display:'flex', flexDirection:'row'}}>
 					<Ionicons
                         name='arrow-up'
                         size={20}
