8d555b13f78a4b90d7c1fcb9f157c6a2313339e9
Dk201833 <dksac333@gmail.com>
2022-01-30 14:31:38 +0545

    validatio message chage

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,10 @@
-export const API_URL = 'https://gc.yakamoztech.com/wp-json';
+export const API_URL = 'https://beta.gilcouncil.com/wp-json';
 export const JWT_TOKEN = 'growth-council-token';
 export const USER_NAME = 'user-name';
 export const USER_AVATAR = 'user-avatar';
 export const FIREBASE_API_KEY = 'AIzaSyC3jlrr6heMtEdVALh2uoWgZtwXFxtExrg';
-export const FIREBASE_AUTH_DOMAIN = '98653816763-iqfs729oiv0ebdh7it2lvdk5as4jbtb6.apps.googleusercontent.com';
+export const FIREBASE_AUTH_DOMAIN =
+  '98653816763-iqfs729oiv0ebdh7it2lvdk5as4jbtb6.apps.googleusercontent.com';
 export const FIREBASE_PROJECT_ID = 'growth-council';
 export const FIREBASE_STORAGE_BUCKET = 'growth-council.appspot.com';
 export const FIREBASE_MESSAGING_SENDER_ID = '98653816763';
