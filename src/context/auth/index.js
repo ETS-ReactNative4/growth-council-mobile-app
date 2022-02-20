cf79ab50ba5275c2413ca19948a703c9e65455c1
Krishna Timilsina <krishna.timilsina@citytech.global>
2022-02-20 16:17:35 +0545

    improvements

--- src/context/auth/index.js
+++ src/context/auth/index.js
@@ -50,9 +50,22 @@ export const AuthProvider = ({children}) => {
                             await setAsyncStorage(USER_NAME, response.data.user_display_name);
                             await setAsyncStorage(USER_AVATAR, response.data.avatar);
                             if (!response?.data?.firebase_password) {
-                                const response = await createUserWithEmailAndPassword(auth, fromData?.username?.trim(), uuid.v4());
-                                const token = await response.user.getIdToken();
-                                console.log('Firebase Token::::', token);
+                                const firebaseUserResponse = await createUserWithEmailAndPassword(auth, fromData?.username?.trim(), uuid.v4());
+                                const firebaseToken = await firebaseUserResponse.user.getIdToken();
+                                console.log('Firebase Token::::', firebaseUserResponse);
+                                const profileResponse = await axios.post(
+                                    API_URL + '}/jwt-auth/v1/users/profile',
+                                    {firebase_password: uuid.v4()},
+                                    {
+                                        headers: {
+                                            Accept: 'application/json',
+                                            'Content-Type': 'application/json',
+                                            'authorization': response?.data?.token,
+                                        },
+                                        responseType: 'json',
+                                    },
+                                );
+                                console.log('Profile response::::', profileResponse);
                             }
                             const firebaseResponse = await signInWithEmailAndPassword(
                                 auth,
