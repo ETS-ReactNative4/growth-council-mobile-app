7e890610051dbb75f835df1cd1c37727ad09a70a
Reshika Shrestha <shresthareshika@gmail.com>
2022-03-01 15:57:53 +0545

    comment remove from Pdf file

--- src/screens/selfLearn/component/pdf.js
+++ src/screens/selfLearn/component/pdf.js
@@ -35,16 +35,13 @@ const pdf = props => {
           },
         );
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          // Start downloading
           downloadFile();
 
           console.log('Storage Permission Granted.');
         } else {
-          // If permission denied then show alert
           Alert.alert('Error', 'Storage Permission Not Granted');
         }
       } catch (err) {
-        // To handle permission related exception
         console.log('++++' + err);
       }
     }
@@ -53,15 +50,13 @@ const pdf = props => {
   const downloadFile = () => {
     // Get today's date to add the time suffix in filename
     let date = new Date();
-    // File URL which we want to download
+
     let FILE_URL = fileUrl;
-    // Function to get extention of the file url
+
     let file_ext = getFileExtention(FILE_URL);
 
     file_ext = '.' + file_ext[0];
 
-    // config: To get response by passing the downloading related options
-    // fs: Root directory path to download
     const {config, fs} = RNFetchBlob;
     let RootDir = fs.dirs.PictureDir;
     let options = {
@@ -74,21 +69,19 @@ const pdf = props => {
           file_ext,
         description: 'downloading file...',
         notification: true,
-        // useDownloadManager works with Android only
         useDownloadManager: true,
       },
     };
     config(options)
       .fetch('GET', FILE_URL, ToastMessage.show('PDF File Download Started.'))
       .then(res => {
-        // Alert after successful downloading
         console.log('res -> ', JSON.stringify(res));
         ToastMessage.show('PDF File Downloaded Successfully.');
       });
   };
 
   const getFileExtention = fileUrl => {
-    // To get the file extension
+
     return /[.]/.exec(fileUrl) ? /[^.]+$/.exec(fileUrl) : undefined;
   };
 
