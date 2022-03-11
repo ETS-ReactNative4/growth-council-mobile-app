d5127951eb12268e6ce36bcd15656210ac6362b5
Reshika Shrestha <shresthareshika@gmail.com>
2022-03-11 20:57:34 +0545

    calendar issue fixed

--- src/screens/calendar/components/index.js
+++ src/screens/calendar/components/index.js
@@ -37,7 +37,7 @@ const EventCalendar = props => {
   const [currentEvents, setCurrentEvents] = useState([]);
   const [showAllEvents, setShowAllEvents] = useState(false);
   const [pickerVisible, setPickerVisible] = useState(false);
-  const [markedDay, setMarkedDay] = useState([]);
+//   const [markedDay, setMarkedDay] = useState([]);
 
   useEffect(() => {
     const fetchCalendarEventAsync = async () => {
@@ -51,76 +51,76 @@ const EventCalendar = props => {
           if (response?.payload?.code === 200) {
             setCurrentEvents(response?.payload?.data);
           } else {
-            setMarkedDay([]);
+            // setMarkedDay([]);
             setCurrentEvents([]);
           }
         })
         .catch(e => {
-          setMarkedDay([]);
+        //   setMarkedDay([]);
           setCurrentEvents([]);
         });
     };
     fetchCalendarEventAsync();
   }, []);
 
-  useEffect(() => {
-    let monthDays = [];
-    currentEvents?.map(item => {
-      const startDate = moment(item.event_start).format('YYYY-MM-DD');
-      const endDate = moment(item.event_end).format('YYYY-MM-DD');
+//   useEffect(() => {
+//     let monthDays = [];
+//     currentEvents?.map(item => {
+//       const startDate = moment(item.event_start).format('YYYY-MM-DD');
+//       const endDate = moment(item.event_end).format('YYYY-MM-DD');
 
-      let backgroundColor = '';
-      const pillarCategory = item?.pillar_categories
-        ? item?.pillar_categories[0]?.parent ||
-          item?.pillar_categories[1]?.parent
-        : '';
-      switch (pillarCategory) {
-        case 0:
-        case 117:
-          backgroundColor = Colors.COMMUNITY_COLOR;
-          break;
-        case 0:
-        case 118:
-          backgroundColor = Colors.PRACTICE_COLOR;
-          break;
-        default:
-          backgroundColor = Colors.COACHING_COLOR;
-      }
+//       let backgroundColor = '';
+//       const pillarCategory = item?.pillar_categories
+//         ? item?.pillar_categories[0]?.parent ||
+//           item?.pillar_categories[1]?.parent
+//         : '';
+//       switch (pillarCategory) {
+//         case 0:
+//         case 117:
+//           backgroundColor = Colors.COMMUNITY_COLOR;
+//           break;
+//         case 0:
+//         case 118:
+//           backgroundColor = Colors.PRACTICE_COLOR;
+//           break;
+//         default:
+//           backgroundColor = Colors.COACHING_COLOR;
+//       }
 
-      if (moment(startDate).isSame(endDate)) {
-        monthDays[startDate] = {
-          color: backgroundColor,
-          textColor: 'white',
-        };
-      } else {
-        const dates = getDates(
-          new Date(moment(startDate).format('YYYY-MM-DD')),
-          new Date(moment(endDate).format('YYYY-MM-DD')),
-        );
-        dates.map((item, index) => {
-          if (index === 0) {
-            monthDays[moment(item).format('YYYY-MM-DD')] = {
-              startingDay: true,
-              color: backgroundColor,
-              textColor: 'white',
-            };
-          } else if (dates?.length - 1 === index) {
-            monthDays[moment(item).format('YYYY-MM-DD')] = {
-              endingDay: true,
-              color: backgroundColor,
-              textColor: 'white',
-            };
-          } else {
-            monthDays[moment(item).format('YYYY-MM-DD')] = {
-              color: backgroundColor,
-              textColor: 'white',
-            };
-          }
-        });
-      }
-      setMarkedDay(monthDays);
-    });
-  }, [currentEvents]);
+//       if (moment(startDate).isSame(endDate)) {
+//         monthDays[startDate] = {
+//           color: backgroundColor,
+//           textColor: 'white',
+//         };
+//       } else {
+//         const dates = getDates(
+//           new Date(moment(startDate).format('YYYY-MM-DD')),
+//           new Date(moment(endDate).format('YYYY-MM-DD')),
+//         );
+//         dates.map((item, index) => {
+//           if (index === 0) {
+//             monthDays[moment(item).format('YYYY-MM-DD')] = {
+//               startingDay: true,
+//               color: backgroundColor,
+//               textColor: 'white',
+//             };
+//           } else if (dates?.length - 1 === index) {
+//             monthDays[moment(item).format('YYYY-MM-DD')] = {
+//               endingDay: true,
+//               color: backgroundColor,
+//               textColor: 'white',
+//             };
+//           } else {
+//             monthDays[moment(item).format('YYYY-MM-DD')] = {
+//               color: backgroundColor,
+//               textColor: 'white',
+//             };
+//           }
+//         });
+//       }
+//       setMarkedDay(monthDays);
+//     });
+//   }, [currentEvents]);
 
   const getDates = (startDate, endDate) => {
     const dates = [];
@@ -136,6 +136,61 @@ const EventCalendar = props => {
     }
     return dates;
   };
+
+  let markedDay = {};
+  currentEvents?.map(item => {
+    const startDate = moment(item?.event_start).format('YYYY-MM-DD');
+    const endDate = moment(item?.event_end).format('YYYY-MM-DD');
+
+    let backgroundColor = '';
+    const pillarCategory = item?.pillar_categories
+      ? item?.pillar_categories[0]?.parent || item?.pillar_categories[1]?.parent
+      : '';
+    switch (pillarCategory) {
+      case 0:
+      case 117:
+        backgroundColor = Colors.COMMUNITY_COLOR;
+        break;
+      case 0:
+      case 118:
+        backgroundColor = Colors.PRACTICE_COLOR;
+        break;
+      default:
+        backgroundColor = Colors.COACHING_COLOR;
+    }
+
+    if (moment(startDate).isSame(endDate)) {
+      markedDay[startDate] = {
+        color: backgroundColor,
+        textColor: 'white',
+      };
+    } else {
+      const dates = getDates(
+        new Date(moment(startDate).format('YYYY-MM-DD')),
+        new Date(moment(endDate).format('YYYY-MM-DD')),
+      );
+      dates.map((item, index) => {
+        if (index === 0) {
+          markedDay[moment(item).format('YYYY-MM-DD')] = {
+            startingDay: true,
+            color: backgroundColor,
+            textColor: 'white',
+          };
+        } else if (dates?.length - 1 === index) {
+          markedDay[moment(item).format('YYYY-MM-DD')] = {
+            endingDay: true,
+            color: backgroundColor,
+            textColor: 'white',
+          };
+        } else {
+          markedDay[moment(item).format('YYYY-MM-DD')] = {
+            color: backgroundColor,
+            textColor: 'white',
+          };
+        }
+      });
+    }
+  });
 
 
   const renderItem = ({item, index}) => {
@@ -288,14 +343,14 @@ const EventCalendar = props => {
                     if (response?.payload?.code === 200) {
                       setCurrentEvents(response?.payload?.data);
                     } else {
-                      setMarkedDay([]);
+                    //   setMarkedDay([]);
                       setCurrentEvents([]);
                     }
                   })
                   .catch(e => {
                     //   ToastMessage.show(e?.response?.payload?.response);
                     console.log(e);
-                    setMarkedDay([]);
+                    // setMarkedDay([]);
                     setCurrentEvents([]);
                   });
               }}
@@ -364,14 +419,14 @@ const EventCalendar = props => {
                           if (response?.payload?.code === 200) {
                             setCurrentEvents(response?.payload?.data);
                           } else {
-                            setMarkedDay([]);
+                            // setMarkedDay([]);
                             setCurrentEvents([]);
                           }
                         })
                         .catch(e => {
                           //   ToastMessage.show(e?.response?.payload?.response);
                           console.log(e);
-                          setMarkedDay([]);
+                        //   setMarkedDay([]);
                           setCurrentEvents([]);
                         });
                     }}>
