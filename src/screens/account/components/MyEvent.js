f56c680bd2ef83f96e48b11accb8bd4178e21397
Reshika Shrestha <shresthareshika@gmail.com>
2022-03-27 14:33:02 +0545

    fontWeight in latest content and time in poe of profile

--- src/screens/account/components/MyEvent.js
+++ src/screens/account/components/MyEvent.js
@@ -11,7 +11,6 @@ import Ionicon from 'react-native-vector-icons/Ionicons';
 import {Button} from 'native-base';
 import moment from 'moment';
 import {useIsFocused} from '@react-navigation/native';
-
 import {CommonStyles, Typography} from '../../../theme';
 import {getAsyncStorage} from '../../../utils/storageUtil';
 import {JWT_TOKEN} from '../../../constants';
@@ -19,6 +18,8 @@ import {decodeUserID} from '../../../utils/jwtUtil';
 import {PRIMARY_BACKGROUND_COLOR} from '../../../theme/colors';
 import {BubblesLoader} from 'react-native-indicator';
 import * as Colors from '../../../theme/colors';
+import * as RNLocalize from 'react-native-localize';
+import {formatTimeByOffset} from '../../event/components/timezone';
 
 const Profile = props => {
   const isFocused = useIsFocused();
@@ -64,6 +65,19 @@ const Profile = props => {
     } else {
       description = item?.organizer?.description;
     }
+	
+    const backStartTimeStamp = item?.event_start;
+    const deviceTimeZone = RNLocalize.getTimeZone();
+
+    const today = moment().tz(deviceTimeZone);
+    const currentTimeZoneOffsetInHours = today.utcOffset() / 60;
+
+    let convertedToLocalTime = formatTimeByOffset(
+      backStartTimeStamp,
+      currentTimeZoneOffsetInHours,
+    );
+   
+    const time = moment(convertedToLocalTime).format('h:mma');
 
     return (
       <View key={index}>
@@ -80,9 +94,7 @@ const Profile = props => {
 
                 <Ionicon name={'time'} size={20} color="#0B0B45" />
                 <Text style={[styles.text, {fontSize: 12}]}>
-                  {item?.event_meta?._start_hour[0]}:
-                  {item?.event_meta?._start_minute[0]}
-                  {item?.event_meta?._start_ampm[0]}
+                  {time}
                 </Text>
               </View>
               <View style={styles.iconWrapper}>
