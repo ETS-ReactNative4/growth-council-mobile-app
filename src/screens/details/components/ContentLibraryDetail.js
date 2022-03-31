b6d44a767649b737031ee174eea018be85a4f4c8
Reshika Shrestha <shresthareshika@gmail.com>
2022-03-31 17:53:33 +0545

    Content library pdf should be open

--- src/screens/details/components/ContentLibraryDetail.js
+++ src/screens/details/components/ContentLibraryDetail.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 import {
   View,
   Text,
@@ -8,6 +8,9 @@ import {
   StyleSheet,
   FlatList,
   PermissionsAndroid,
+  Button,
+  StatusBar,
+  Dimensions,
 } from 'react-native';
 
 import FeatherIcon from 'react-native-vector-icons/Feather';
@@ -22,8 +25,11 @@ import SearchHeader from '../../../shared/header/SearchHeader';
 import {Colors, CommonStyles} from '../../../theme';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Searchbar} from 'react-native-paper';
+import {useFocusEffect, useIsFocused} from '@react-navigation/native';
+
 import {BubblesLoader} from 'react-native-indicator';
 import WebView from 'react-native-autoheight-webview';
+import HTMLView from 'react-native-htmlview';
 
 const ContentLibraryDetail = props => {
   const {
@@ -36,24 +42,26 @@ const ContentLibraryDetail = props => {
     cleanContentLibraryDetail,
   } = props;
 
-  useEffect(() => {
-    const fetchContentLibraryDetailAsync = async () => {
-      await fetchContentLibraryDetail(route?.params?.id);
-    };
-    fetchContentLibraryDetailAsync();
-  }, []);
+  const isFocused = useIsFocused();
+  useFocusEffect(
+    useCallback(() => {
+      fetchContentLibraryDetail(route?.params?.id);
+      return () => {
+        cleanContentLibraryDetail();
+      };
+    }, [isFocused]),
+  );
 
   const [isTrue, setIsTrue] = useState(true);
-  const [searchText, setSearchText] = useState('');
-  const [searchKey, setSearchKey] = useState('');
 
   const handleFeedbackChange = value => {
     setIsTrue(value);
   };
 
-  const _renderItem = ({item, index}) => {
-    const source = {uri: item?.file?.url, cache: true};
+  console.log(route.params.id);
+  console.log({contentLibraryDetails});
 
+  const _renderItem = ({item, index}) => {
     const fileUrl = item?.file?.url;
 
     const checkPermission = async () => {
@@ -118,6 +126,10 @@ const ContentLibraryDetail = props => {
       return /[.]/.exec(fileUrl) ? /[^.]+$/.exec(fileUrl) : undefined;
     };
     return (
+		<TouchableOpacity
+        onPress={() =>
+          navigation.navigate('pdf', {paramsFile: item?.file?.url})
+        }>
       <View style={styles.attachmentContainer}>
         <View style={{flex: 1, flexDirection: 'row'}}>
           <FontAwesomeIcon name="file-pdf-o" size={35} color="#9B9CA0" />
@@ -130,56 +142,91 @@ const ContentLibraryDetail = props => {
           <FeatherIcon name="arrow-down" size={20} color="#9B9CA0" />
         </TouchableOpacity>
       </View>
+	  </TouchableOpacity>
     );
   };
+
+  const _renderTagItem = ({item, index}) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('ContentTags', {
+            itemname: item?.name,
+            title: route?.params?.title,
+            id: item?.term_id,
+          })
+        }>
+        <View style={styles.tagsContainer}>
+          <View style={styles.singleTagContainer}>
+            <FeatherIcon
+              name="tag"
+              size={20}
+              color="#9B9CA0"
+              style={{marginTop: 5}}
+            />
+            <Text style={styles.tagTitleText} numberOfLines={2}>
+              {item?.name}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  const _renderActionItem = ({item, index}) => {
+    return (
+      <View style={{marginBottom: 10, flexDirection: 'row'}}>
+        <Entypo name="dot-single" size={20} color="black" />
+
+        <Text
+          style={{
+            fontFamily: 'SFProText-Regular',
+            color: Colors.SECONDARY_TEXT_COLOR,
+          }}>
+          {item?.list}
+        </Text>
+      </View>
+    );
+  };
+  console.log(contentLibraryDetails?.presenter);
+  let video = contentLibraryDetails?.video_url;
+  if (video !== undefined) {
+    video = contentLibraryDetails?.video_url;
+  } else {
+    video = '';
+  }
   return (
     <View style={styles.container}>
-      {/* Header Section */}
-      {/* <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          marginTop: 20,
-          alignContent: 'center',
-          marginLeft: 10,
-        }}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons
-            name="chevron-back-outline"
-            size={30}
-            color="#B2B3B9"
-            style={{marginTop: 5}}
-          />
-        </TouchableOpacity>
-
-        <Searchbar
-          style={styles.input}
-          placeholder="Search"
-          keyboardType="default"
-          value={searchKey}
-          onChangeText={async text => {
-            setSearchKey(text);
-            //   await fetchAllUsers({
-            // 	s: text,
-            // 	sort: sorting,
-            // 	expertise_areas: category,
-            //   });
-          }}
-        />
-      </View> */}
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+        backgroundColor="grey"
+        translucent={false}
+      />
 
       <View style={styles.bodyContainer}>
         {/* Breadcrumb Section */}
         <View style={styles.breadcrumbContainer}>
           <View style={styles.singleBreadcrumb}>
-            <Text style={styles.inactiveBreadcrumbText}>
-              Customer Experience Ecosystem
-            </Text>
+           
+            {route.params.itemname !== undefined && (
+              <HTMLView
+                value={route.params.itemname}
+                textComponentProps={{
+                  style: {
+                    marginRight: 10,
+                    fontFamily: 'SFProText-Medium',
+                    fontSize: 8,
+                    color: '#B2B3B9',
+                  },
+                }}
+              />
+            )}
+
             <FeatherIcon name="chevron-right" size={10} color="#B2B3B9" />
           </View>
           <View style={{...styles.singleBreadcrumb, flex: 1}}>
             <Text style={styles.activeBreadcrumbText}>
-              {route.params.title}
+              {route?.params?.title}
             </Text>
           </View>
         </View>
@@ -188,105 +235,107 @@ const ContentLibraryDetail = props => {
         <ScrollView
           showsVerticalScrollIndicator={false}
           style={{padding: 25}}
-          contentContainerStyle={{paddingBottom: 90}}>
-          <View style={{marginBottom: 30}}>
-            {/* <Image
-              source={require('../../../assets/img/image.png')}
-              style={styles.contentImage}
-            /> */}
-            <WebView
-              style={{width: '100%', height: 205, borderRadius: 10}}
-              allowsFullscreenVideo
-              scrollEnabled={false}
-              automaticallyAdjustContentInsets
-              source={{
-                html: `
-          <html>
+          contentContainerStyle={{paddingBottom: 60}}>
+          {contentLibraryDetails?.video_url !== null &&
+            contentLibraryDetails?.video_url !== '' && (
+              <View style={{height: 180, borderRadius: 10}}>
+                <WebView
+                  style={{width: '100%', height: 205, marginTop: 10}}
+                  allowsFullscreenVideo
+                  scrollEnabled={false}
+                  webViewStyle={{opacity: 0.99}}
+                  automaticallyAdjustContentInsets
+                  source={{
+                    html: `
+          <Html>
             <body>
-              ${contentLibraryDetails?.video_url}
+              ${video}
             </body>
-          </html>
+          </Html>
         `,
-              }}
-            />
-          </View>
-
-          {/* <View style={styles.sectionContainerBorder}>
-            <Text style={styles.bodyTitleText}>Presented By:</Text>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Image
-                source={require('../../../assets/img/connection-image.png')}
-                style={styles.userImage}
-              />
-              <View style={{marginLeft: 20}}>
-                <Text style={styles.userNameText}>Michael "Coop" Cooper</Text>
-                <Text style={styles.userInfoText}>
-                  Founder, Innovators + Influencers
-                </Text>
+                  }}
+                />
               </View>
-            </View>
-          </View> */}
+            )}
+          {contentLibraryDetails?.presenter !== false &&
+            contentLibraryDetails?.presenter !== null &&
+            contentLibraryDetails?.presenter !== '' && (
+              <View style={styles.sectionContainerBorder}>
+                <Text style={styles.bodyTitleText}>Presented By:</Text>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Image
+                    source={{uri: contentLibraryDetails?.presenter_image}}
+                    style={styles.userImage}
+                  />
+                  <View style={{marginLeft: 20}}>
+                    <Text style={styles.userNameText}>
+                      {contentLibraryDetails?.presenter}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            )}
+
           {contentLibraryDetailsLoading && (
             <View style={styles.loading1}>
               <BubblesLoader color={Colors.SECONDARY_TEXT_COLOR} size={80} />
             </View>
           )}
           {/* Abstract Section */}
-          <View style={styles.sectionContainer}>
-            <Text style={styles.bodyTitleText}>Abstract:</Text>
-            <Text style={styles.abstractDescriptionText}>
-              {contentLibraryDetails?.abstract}
-            </Text>
-          </View>
-
-          {/* Call To Action Section */}
-          <View style={styles.sectionContainer}>
-            <Text style={styles.bodyTitleText}>Call to Action:</Text>
-            {contentLibraryDetails?.call_to_action?.map(item => (
-              <View style={{marginBottom: 10, flexDirection: 'row'}}>
-                <Entypo name="dot-single" size={20} color="black" />
-
-                <Text
-                  style={{
-                    fontFamily: 'SFProText-Regular',
-                    color: Colors.SECONDARY_TEXT_COLOR,
-                  }}>
-                  {item.list}
+          {contentLibraryDetails?.abstract !== undefined &&
+            contentLibraryDetails?.abstract !== '' && (
+              <View style={styles.sectionContainer}>
+                <Text style={styles.bodyTitleText}>Abstract:</Text>
+                <Text style={styles.abstractDescriptionText}>
+                  {contentLibraryDetails?.abstract}
                 </Text>
               </View>
-            ))}
-          </View>
+            )}
+          {/* Call To Action Section */}
+          {contentLibraryDetails?.call_to_action?.length !== 0 &&
+            contentLibraryDetails?.call_to_action !== false && (
+              <View style={styles.sectionContainer}>
+                <Text style={styles.bodyTitleText}>Call to Action:</Text>
+                <FlatList
+                  showsHorizontalScrollIndicator={false}
+                  showsVerticalScrollIndicator={false}
+                  data={contentLibraryDetails?.call_to_action}
+                  renderItem={_renderActionItem}
+                />
+              </View>
+            )}
 
           {/* Attachments Section */}
-          <View style={styles.sectionContainer}>
-            <Text style={styles.bodyTitleText}>Attachments:</Text>
-            <FlatList
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}
-              data={contentLibraryDetails?.attachment}
-              renderItem={_renderItem}
-            />
-          </View>
+          {contentLibraryDetails?.attachment?.length !== 0 &&
+            contentLibraryDetails?.attachment !== false && (
+              <View style={styles.sectionContainer}>
+                <Text style={styles.bodyTitleText}>Attachments:</Text>
+                <FlatList
+                  showsHorizontalScrollIndicator={false}
+                  showsVerticalScrollIndicator={false}
+                  data={contentLibraryDetails?.attachment}
+                  renderItem={_renderItem}
+                />
+              </View>
+            )}
 
           {/* Tags Section */}
-          <View style={styles.sectionContainerBorder}>
-            <Text style={styles.bodyTitleText}>Tags:</Text>
-            <View style={styles.tagsContainer}>
-              {contentLibraryDetails?.tags?.map(item => (
-                <View style={styles.singleTagContainer}>
-                  <FeatherIcon
-                    name="tag"
-                    size={20}
-                    color="#9B9CA0"
-                    style={{marginTop: 5}}
-                  />
-                  <Text style={styles.tagTitleText} numberOfLines={2}>
-                    {item?.name}
-                  </Text>
-                </View>
-              ))}
+          {contentLibraryDetails?.tags?.length !== 0 && (
+            <View style={styles.sectionContainerBorder}>
+              <Text style={styles.bodyTitleText}>Tags:</Text>
+
+              <FlatList
+                contentContainerStyle={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                }}
+                showsVerticalScrollIndicator={false}
+                data={contentLibraryDetails?.tags}
+                renderItem={_renderTagItem}
+              />
             </View>
-          </View>
+          )}
 
           {/* Article Feedback Section */}
           <ArticleFeedbackCard
@@ -304,7 +353,6 @@ const ContentLibraryDetail = props => {
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     ...CommonStyles.container,
@@ -341,10 +389,11 @@ const styles = StyleSheet.create({
   contentImage: {width: '100%', height: 205, borderRadius: 16},
   sectionContainerBorder: {
     margin: 2,
-    marginBottom: 20,
+    marginBottom: 10,
     paddingBottom: 25,
     borderBottomWidth: 1,
     borderColor: 'rgba(112, 112, 112, 0.13)',
+    marginTop: 20,
   },
   bodyTitleText: {
     marginBottom: 20,
@@ -360,7 +409,8 @@ const styles = StyleSheet.create({
   userNameText: {
     marginBottom: 10,
     fontFamily: 'SFProText-Medium',
-    color: Colors.PRIMARY_TEXT_COLOR,
+    color: Colors.SECONDARY_TEXT_COLOR,
+    width: '75%',
   },
   userInfoText: {
     fontFamily: 'SFProText-Medium',
@@ -368,6 +418,7 @@ const styles = StyleSheet.create({
   },
   sectionContainer: {
     marginBottom: 20,
+    marginTop: 20,
   },
   abstractDescriptionText: {
     flex: 1,
@@ -414,11 +465,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   singleTagContainer: {
-    width: '49%',
-    height: 47,
+    width: (Dimensions.get('window').width - 90) / 2,
+    height: 50,
     marginBottom: 10,
-    paddingHorizontal: 15,
+    marginLeft: 5,
+    marginRight: 5,
+    paddingHorizontal: 5,
     flexDirection: 'row',
+    justifyContent: 'center',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
@@ -433,7 +487,7 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 8,
     fontFamily: 'SFProText-Regular',
-    fontSize: 10,
+    fontSize: 9,
     lineHeight: 15,
     color: Colors.SECONDARY_TEXT_COLOR,
   },
