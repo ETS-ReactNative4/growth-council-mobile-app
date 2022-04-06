e1c62a1c354ab0513ab6ced8daa25350c0e4a1ff
ecv-harihopal <harihopal@classifiedsventures.com>
2022-04-07 01:14:16 +0545

    set poe image resize mode as contain

--- src/screens/dashboard/components/BestPractice.js
+++ src/screens/dashboard/components/BestPractice.js
@@ -213,6 +213,7 @@ const BestPractice = props => {
             <Image
               source={{uri: item?.image}}
               style={{width: 30, height: 30}}
+              resizeMode="contain"
             />
           </View>
           <Text
