52b23220faa048bf76fb4c278c7da550da91f725
Reshika Shrestha <shresthareshika@gmail.com>
2022-05-08 20:06:13 +0545

    comment remove from dashboard

--- src/screens/dashboard/components/index.js
+++ src/screens/dashboard/components/index.js
@@ -91,18 +91,9 @@ const Dashboard = props => {
 
   useEffect(() => {
     const backAction = () => {
-      //   Alert.alert("Hold on!", "Are you sure you want to go back?", [
-      //     {
-      //       text: "Cancel",
-      //       onPress: () => null,
-      //       style: "cancel"
-      //     },
-      //     { text: "YES", onPress: () => BackHandler.exitApp() }
-      //   ]);
-      //   return true;
       BackHandler.exitApp();
     };
-
+	
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
       backAction,
