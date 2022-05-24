c744880e6519de315260b55e3e51e5b50919cf30
Reshika Shrestha <shresthareshika@gmail.com>
2022-05-24 18:50:46 +0545

    console terms slice and url change of new member

--- src/screens/terms/index.js
+++ src/screens/terms/index.js
@@ -2,33 +2,33 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import Terms from './component/index';
-import { fetchTerms, resetTerms } from './component/termsSlice';
+// import { fetchTerms, resetTerms } from './component/termsSlice';
 
 
 const TermScreen = props => {
   const dispatch = useDispatch();
-  const {terms, termsLoading, termsError} = useSelector(
-    state => state.terms,
-  );
+//   const {terms, termsLoading, termsError} = useSelector(
+//     state => state.terms,
+//   );
 
-  const fetchTermsOfUse = () => {
-    dispatch(fetchTerms());
-  };
+//   const fetchTermsOfUse = () => {
+//     dispatch(fetchTerms());
+//   };
 
-  const cleanTerms = () => {
-    dispatch(resetTerms());
-  };
+//   const cleanTerms = () => {
+//     dispatch(resetTerms());
+//   };
 
 
   return (
     <Terms
       {...props}
 
-	  terms={terms}
-	  termsLoading={termsLoading}
-	  termsError={termsError}
-	  fetchTermsOfUse={fetchTermsOfUse}
-	  cleanTerms={cleanTerms}
+	//   terms={terms}
+	//   termsLoading={termsLoading}
+	//   termsError={termsError}
+	//   fetchTermsOfUse={fetchTermsOfUse}
+	//   cleanTerms={cleanTerms}
      
     />
   );
